4c2dcae5c34e58eac5532daa6c14ad5afb9051c2
amos <calcio.vita@gmail.com>
2020-08-24 15:27:45 +0000

    10.5出勤登録ボタン完了

---
+++ app/controllers/attendances_controller.rb
@@ -0,0 +1,16 @@
+class AttendancesController < ApplicationController
+  
+  def update
+    @user = User.find(params[:user_id])
+    @attendance = Attendance.find(params[:id])
+    
+    if @attendance.started_at.nil?
+      if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
+        flash[:ingo] = "おはようございます！"
+      else
+        flash[:danger] = "勤怠登録失敗しました。"
+      end
+    end
+    redirect_to @user
+  end  
+end
