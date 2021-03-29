4b182962ba7ccace87418d8516894351af1e3e4d
EC2 Default User <ec2-user@ip-172-31-29-173.ap-northeast-1.compute.internal>
2021-03-29 11:48:58 +0000

    勤怠編集画面の修正

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -4,13 +4,24 @@ class Attendance < ApplicationRecord
   validates :worked_on, presence: true
   validates :note, length: { maximum: 50 }
   
+ 
+  
    # 出勤時間が存在しない場合、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at
   
   validate :started_at_than_finished_at_fast_if_invalid
+  
+  
 
   def finished_at_is_invalid_without_a_started_at
-     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
+    if ( !started_at.blank? && finished_at.blank?) || ( started_at.blank? && !finished_at.blank?)
+      errors.add(:started_atm,'test') 
+    end
+    
+   
+    # if !started_at.blank? && started_at.present?
+    #   errors.add(:started_at, "が必要です") if !started_at.present? 
+    # end
   end
   
   def started_at_than_finished_at_fast_if_invalid
